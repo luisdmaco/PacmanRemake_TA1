5eea006878da231e8e4f0912b8e9e7ead6f77af3
luisdmaco <ldmaco09@outlook.com>
2021-09-20 14:40:56 -0500

    primer cout

---
+++ PacmanRemake/main.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<conio.h>
+#include "Pacman.h"
+#include "Fantasma.h"
+#include "Pacdots.h"
+#include "Bonus.h"
+
+using namespace std;
+using namespace System;
+
+int main() {
+	cout << "Hello world!";
+	_getch();
+	return 0;
+}
